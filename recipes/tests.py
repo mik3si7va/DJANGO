d652f660dd07356b0aff5db87f54e622986792de
mik3si7va <mik3si7va@gmail.com>
2026-01-07 22:08:09 +0000

    initializing tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from recipes.models import Category, Recipe
+
+
+class RecipeURLsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user1', password='pass1234')
+        self.category = Category.objects.create(name='Category 1')
+        self.recipe = Recipe.objects.create(
+            title='Recipe 1',
+            description='Recipe description',
+            slug='recipe-1',
+            preparation_time=10,
+            servings=2,
+            preparation_steps='Do the thing',
+            is_published=True,
+            category=self.category,
+            author=self.user,
+        )
+
+    def test_home_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_detail_url(self):
+        response = self.client.get(f'/recipe/{self.recipe.id}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_url(self):
+        response = self.client.get(f'/category/{self.category.id}/')
+        self.assertEqual(response.status_code, 200)
